8ce66b0b3608941a2939bb4e7a2a67472fab9dbf
Maisa Preis <maisapierinipreis@gmail.com>
2024-01-23 20:40:11 -0300

    fix: Fixed an issue that was being highlighted by the console

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('define').tap((args) => {
+      args[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = true;
+      return args;
+    });
+  },
+  transpileDependencies: true,
+};
