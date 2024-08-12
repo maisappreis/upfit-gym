303bd4ec1cb6d9dd76bb15d3218dece4d52c9bf0
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-12 19:53:31 -0300

    feat: Creating environment variable.

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -2,7 +2,7 @@
 export const globalVariablesMixin = {
     data() {
         return {
-            apiURL: "http://localhost:8000/api",
+            apiURL: import.meta.env.VITE_API_URL,
             months: [
                 "Janeiro",
                 "Fevereiro",
