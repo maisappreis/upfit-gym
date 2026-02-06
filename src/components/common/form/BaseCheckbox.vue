32ca204d34fd51dec094b9234da9fd9c910e8acf
Maisa Preis <maisapierinipreis@gmail.com>
2026-02-06 17:10:16 -0300

    feat: Created checkbox component.

---
+++ src/components/common/form/BaseCheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form-item">
+    <label
+      v-if="label"
+      :for="String(uuid)"
+      class="form-label"
+    >
+      {{ label }}
+    </label>
+    <input
+      v-bind="{ ...$attrs, onChange: updateValue }"
+      :checked="modelValue"
+      :id="String(uuid)"
+      type="checkbox"
+    />
+  </div>
+  
+  <BaseErrorMessage
+    v-if="error"
+    :id="`${uuid}-error`"
+  >
+    {{ error }}
+  </BaseErrorMessage>
+</template>
+
+<script setup lang="ts">
+import UniqueID from "@/features/UniqueID";
+import SetupFormComponent from "@/features/SetupFormComponent";
+import BaseErrorMessage from "@/components/common/BaseErrorMessage.vue";
+
+const props = defineProps<{
+  label?: string
+  error?: string
+  modelValue?: boolean
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string | number | boolean | null): void
+}>();
+
+const { updateValue } = SetupFormComponent(props, { emit });
+const uuid = UniqueID().getID();
+</script>
