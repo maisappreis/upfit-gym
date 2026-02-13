71964e76afc224d187e53c726a9dcde3b42a6c06
Maisa Preis <maisapierinipreis@gmail.com>
2026-02-13 20:54:14 -0300

    refactor: Creating a reusable and scalable modal component, and applying it to the components: CustomerPage, ExpensePage and RevenuePage.

--- src/components/content/ExpensesPage.vue
+++ src/components/content/ExpensesPage.vue
@@ -20,30 +20,58 @@
     <ExpensesTable
       :data="filteredExpenses"
       :searchedField="searchedField"
-      @update-item="modalCrud.openUpdate($event)"
-      @delete-item="showDeleteModal"
+      @update-item="openUpdateModal($event)"
+      @delete-item="openDeleteModal($event)"
     />
 
-    <ModalCard
-      v-if="modalCrud.isOpen.value"
-      :isForm="modalCrud.isForm.value"
-      @execute-action="deleteExpense"
-      @close-modal="modalCrud.close"
-    >
-      <h3 v-if="modalCrud.isDelete.value" class="message-area">
+    <ModalCard2 v-model="modalCrud.isOpen.value">
+      <template #header>
+        <span>
+          {{ modalTitle }}
+        </span>
+      </template>
+
+      <!-- DELETE CONFIRM -->
+      <p
+        v-if="modalCrud.isDelete.value"
+        class="message-area"
+      >
         Tem certeza que deseja excluir o pagamento da despesa de
-        <strong class="highlight">{{ messageData.name }}</strong> referente ao mês de
-        <strong class="highlight">{{ messageData.date }}</strong
-        >?
-      </h3>
+        <strong class="highlight">{{ selectedExpense?.name }}</strong> referente ao mês de
+        <strong class="highlight">{{ selectedExpense?.month }}</strong> de
+        <strong class="highlight">{{ selectedExpense?.year }}</strong>?
+      </p>
+
+      <!-- CREATE AND UPDATE FORM -->
       <ExpensesForm
         v-else
-        :item="selectedExpense"
-        :action="modalCrud.mode.value"
-        :modalTitle="modalTitle"
-        @close-modal="modalCrud.close"
+        v-model="expenseForm"
+        ref="formRef"
       />
-    </ModalCard>
+
+      <template #footer>
+          <BaseButton
+            v-if="modalCrud.isDelete.value"
+            :loading="loadingStore.isLoading"
+            @click="deleteExpense"
+          >
+            Confirmar
+          </BaseButton>
+          <BaseButton
+            v-else
+            type="submit"
+            size="lg"
+            :disabled="!formRef?.isValid"
+            :loading="loadingStore.isLoading"
+            @click="submitForm"
+          >
+            Salvar
+          </BaseButton>
+          <BaseButton size="lg" variant="danger" @click="modalCrud.close">
+            Cancelar
+          </BaseButton>
+      </template>
+    </ModalCard2>
 
     <div v-if="modalCrud.isOpen.value" class="defocus"></div>
 
@@ -58,14 +86,15 @@ import { useAlertStore } from "@/stores/alert";
 import { useLoadingStore } from "@/stores/loading";
 import { useCrudModal } from "@/composables/useCrudModal";
 import { useDataUtils } from "@/utils/dataUtils";
+import { useDateUtils } from "@/utils/dateUtils";
 import { expenseService } from "@/services/expense.service";
-import { type Expense, type Message } from "@/types/expense";
+import { type Expense, type CreateExpenseDTO } from "@/types/expense";
 
 import BaseButton from "@/components/common/BaseButton.vue";
 import ExpensesTable from "@/components/tables/ExpensesTable.vue";
 import AlertMessage from "@/components/common/AlertMessage.vue";
 import SearchFilter from "@/components/common/SearchFilter.vue";
-import ModalCard from "@/components/common/ModalCard.vue";
+import ModalCard2 from "@/components/common/ModalCard2.vue";
 import MonthFilter from "@/components/common/MonthFilter.vue";
 import ExpensesForm from "@/components/forms/ExpensesForm.vue";
 
@@ -74,14 +103,25 @@ const alertStore = useAlertStore();
 const loadingStore = useLoadingStore();
 
 const modalCrud = useCrudModal<Expense>();
-const { filteredData } = useDataUtils();
+const { filteredData, capitalize } = useDataUtils();
+const { getYearAndMonth }= useDateUtils();
 
 const statusList = ref<string[]>(["Pago", "À pagar", "Todos"]);
 const searchedField = ref<string[]>([]);
-const messageData = ref<Message>({} as Message);
 const currentMonth = ref<string>("");
 const currentYear = ref<number>(0);
 const currentStatus = ref<string>("");
+const formRef = ref<any>(null);
+const expenseForm = ref<CreateExpenseDTO>({
+  name: "",
+  month: "",
+  date: "",
+  paid: "À pagar",
+  value: null,
+  year: 0,
+  installments: "",
+  notes: "",
+});
 
 const selectedExpense = computed(() => modalCrud.entity.value);
 
@@ -89,6 +129,7 @@ const modalTitle = computed(() => {
   switch (modalCrud.mode.value) {
     case "create": return "Adicionar Despesa";
     case "update": return "Atualizar Despesa";
+    case "delete": return "Excluir Despesa";
     default: return "";
   }
 });
@@ -102,15 +143,92 @@ const filteredExpenses = computed(() => {
   ) as Expense[];
 });
 
-const showDeleteModal = (expense: Expense) => {
-  modalCrud.openDelete(expense)
+const closeModal = () => {
+  modalCrud.close();
 
-  let date = `${expense.month}/${expense.year}`;
-  messageData.value = {
-    name: expense.name,
-    date: date,
-    view: "expense"
+  expenseForm.value = {
+     name: "",
+    month: "",
+    date: "",
+    paid: "À pagar",
+    value: null,
+    year: 0,
+    installments: "",
+    notes: "",
   };
+};
+
+const openUpdateModal = (expense: Expense) => {
+  expenseForm.value = {
+    name: expense.name,
+    month: expense.month,
+    date: expense.date,
+    paid: expense.paid,
+    value: expense.value,
+    year: expense.year,
+    installments: expense.installments,
+    notes: expense.notes,
+  };
+
+  modalCrud.openUpdate(expense);
+};
+
+const openDeleteModal = (expense: Expense) => {
+  modalCrud.openDelete(expense)
+};
+
+const submitForm = async () => {
+  if (!formRef.value?.isValid) return;
+
+  let date = getYearAndMonth(expenseForm.value.date);
+
+  const payload = {
+    ...expenseForm.value,
+    name: capitalize(expenseForm.value.name),
+    year: date.year,
+    month: date.month,
+  };
+
+  if (modalCrud.mode.value === "create") {
+    createExpense(payload);
+  } else {
+    updateExpense(
+      selectedExpense.value!.id,
+      payload
+    );
+  }
+};
+
+const createExpense = async (payload: CreateExpenseDTO) => {
+  loadingStore.start();
+
+  try {
+    await expenseService.create(payload);
+    await apiStore.fetchExpenses();
+
+    closeModal();
+    alertStore.success("Despesa criada com sucesso!");
+  } catch (error) {
+    alertStore.error("Erro ao criar despesa.", error);
+  } finally {
+    loadingStore.stop();
+  }
+};
+
+const updateExpense = async (expenseId: number, payload: CreateExpenseDTO) => {
+  loadingStore.start();
+
+  try {
+    await expenseService.update(expenseId, payload);
+    await apiStore.fetchExpenses();
+
+    closeModal();
+    alertStore.success("Despesa atualizada com sucesso!");
+  } catch (error) {
+    alertStore.error("Erro ao atualizar despesa.", error);
+  } finally {
+    loadingStore.stop();
+  }
 };
 
 const deleteExpense = async () => {
