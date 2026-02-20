e1ae3812f1673f7ce652c451a74e0799ec2a0bee
Maisa Preis <maisapierinipreis@gmail.com>
2026-02-20 14:41:43 -0300

    deploy: updating build

--- src/components/views/LayoutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <HeaderPage />
-  <SideBar />
-</template>
-
-<script setup lang="ts">
-import HeaderPage from "@/components/layout/HeaderPage.vue";
-import SideBar from "@/components/layout/SideBar.vue";
-</script>
--- src/services/authClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const authClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
